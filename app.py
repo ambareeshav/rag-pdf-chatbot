161adff6d91f3f88e7489d641d0984b9d5a73799
ambareeshav <126247692+ambareeshav@users.noreply.github.com>
2024-07-02 12:44:57 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,79 @@
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from operator import itemgetter
+from langchain_core.output_parsers import StrOutputParser
+
+#Loads the vectorstore created in data.py
+def load_index():
+  embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large-instruct")
+  db = FAISS.load_local(
+    "faiss_index", 
+    embeddings,  
+    allow_dangerous_deserialization=True #allow_dangerous_deserialization=True; this flag is set to allow loading locally stored indexes that may otherwise be malicious
+  )
+
+  return db
+
+#Creating and configuring chain
+def create_chain(llm, retriever):
+
+  #Template to direct the agent
+  rag_tempalte = """
+  Answer the question based only on the following context:
+  {context}
+  Question: {question}
+  you will also have access to chat history:
+  {chat_history}
+  """
+  #Creating a prompt from the template 
+  rag_prompt = ChatPromptTemplate.from_template(rag_tempalte)
+
+  #The chain is configured to take inputs for chat history and question
+  chain = (
+      {
+          "chat_history": itemgetter("chat_history"),
+          "context": itemgetter("question") | retriever,#the question is chained to the retriever that then returns k relevant documents from the vector database
+          "question": itemgetter("question")
+
+      }
+      | rag_prompt
+      | llm
+      | StrOutputParser()
+  )
+
+  return chain
+
+
+def chatbot(chain):
+
+  #List to store chat history
+  ch = []
+  while True:
+    q = input("User :  ")
+    if "exit" in q:
+      break
+
+    #Get user query and invoke chain with query and chat history
+    res = chain.invoke({"chat_history": itemgetter(ch), "question": q})
+    print("Llama : ", res)
+
+    #Add current conversation to chat history
+    ch.append((q, res))
+
+def main():
+
+  #loading the db and creating a retriever class
+  db = load_index()
+  retriever = db.as_retriever()
+
+  #Initializing llama using groq api
+  llm = ChatGroq(
+    model_name = 'llama3-70b-8192'
+    )
+  chain = create_chain(llm,retriever)
+  chatbot(chain)
+
+if __name__=="__main__":
+    main()
---
+++ data.py
@@ -0,0 +1,51 @@
+"""
+In this file, we read the pdf, convert it to text, chunk it and embedd it in a FAISS vector database
+The db is stored as faiss_index locally to be loaded in the main file
+"""
+
+import PyPDF2
+from transformers import GPT2TokenizerFast
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+pdf_path = 'attention_is_all_you_need.pdf'
+output_txt = 'attention_is_all_you_need.txt'
+
+# Open the PDF file in read-binary mode
+with open(pdf_path, 'rb') as pdf_file:
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+    # Initialize an empty string to store the text
+    text = ''
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+
+# Write the extracted text to a text file and open it in read mode
+with open(output_txt, 'w', encoding='utf-8') as txt_file:
+    txt_file.write(text)
+
+with open(output_txt, 'r', encoding='utf-8') as txt_file:
+    text = txt_file.read()
+
+#initializing function for counting tokens in txt file
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+def count_tokens(text: str) -> int:
+    return len(tokenizer.encode(text))
+
+#chunking the text
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 256,
+    chunk_overlap  = 24,
+    length_function = count_tokens,
+)
+chunks = text_splitter.split_text(text)
+
+#generate vector emdeddings for the text and store them in a FAISS vector database
+embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large-instruct")
+
+db = FAISS.from_texts(texts=chunks,embedding=embeddings)
+
+db.save_local("faiss_index")
+print("Index saved!")
